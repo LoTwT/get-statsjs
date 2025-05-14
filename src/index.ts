9657489ff98657ee842c4016cd2ed36a4edc1836
Lo <l709937065@gmail.com>
2025-05-14 11:07:23 +0800

    feat: add getStatsjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const one = 1
+import Stats from "stats.js"
+
+interface GetStatsjsReturnType {
+  stats: Stats
+  mount: () => void
+  dispose: () => void
+}
+
+export function getStatsjs(el?: HTMLElement): GetStatsjsReturnType {
+  const stats = new Stats()
+
+  const mount = (): void => {
+    if (!el) el = document.body
+    el.appendChild(stats.dom)
+  }
+
+  const dispose = (): void => {
+    if (!el) el = document.body
+    el.removeChild(stats.dom)
+  }
+
+  return { stats, mount, dispose }
+}
